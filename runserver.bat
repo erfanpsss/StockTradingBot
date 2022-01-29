73d9dde4920d339991855f21b6c5adf74a80bc4e
erfan <erfanpsss@hotmail.com>
2022-01-29 00:15:01 +0000

    edited runserver

--- runserver.bat
+++ runserver.bat
@@ -1,2 +1,1 @@
-C:\Users\Administrator\Documents\StockTradingApp\StockTradingBotPythonVenv\Scripts\activate
-python C:\Users\Administrator\Documents\StockTradingApp\StockTradingBot\stocktrading_root\manage.py runserver 127.0.0.1:8888
+C:\Users\Administrator\Documents\StockTradingApp\StockTradingBotPythonVenv\Scripts\activate && python C:\Users\Administrator\Documents\StockTradingApp\StockTradingBot\stocktrading_root\manage.py runserver 127.0.0.1:8888
